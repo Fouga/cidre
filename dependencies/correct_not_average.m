d5d63ce0a12ad1cb088490f8598a72c4f320ad20
Fouga <natalia.chicherova@gmail.com>
2017-07-10 14:35:41 +0200

    Excluded darkfield from correction. However the choice to use it is still available

--- dependencies/correct_not_average.m
+++ dependencies/correct_not_average.m
@@ -1,7 +1,18 @@
-function imCorrected = correct_not_average(im,model)
+function imCorrected = correct_not_average(im,model,varargin)
 
-z  = model.z;
-v = model.v;
-imCorrected = ((double(im) - repmat(z,1,1,size(im,3)))./repmat(v,1,1,size(im,3)));
-imCorrected = uint16(imCorrected);
-
+if nargin==2
+    method = 'all_corrections';
+else
+    method = varargin{1};
+end
+switch method
+    case 'all_corrections'
+        z  = model.z;
+        v = model.v;
+        imCorrected = ((double(im) - repmat(z,1,1,size(im,3)))./repmat(v,1,1,size(im,3)));
+        imCorrected = uint16(imCorrected);
+    case 'flatfield'
+        v = model.v;
+        imCorrected = (double(im)./repmat(v,1,1,size(im,3)));
+        imCorrected = uint16(imCorrected);
+end
